eb7d356ca0f5e28229568f604e62755857394ef0
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2023-11-15 06:40:21 +0000

    new shell update

--- simple_shell.c
+++ simple_shell.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,57 +8,35 @@
 #define BUFFER_SIZE 1024
 
 /**
-* main - Simple UNIX command line interpreter.
-*
-* Return: 0 on successful execution, -1 on failure.
-*
-* This program acts as a basic shell,
-* allowing users to input commands,
-* displaying a prompt, and executing
-* the entered command. It supports
-* basic functionalities such as handling
-* command lines with arguments,
-* managing the PATH, implementing built-in
-zi commands like `exit` and `env`,
-*  and more.
-*/
-
-
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
-
-#define BUFFER_SIZE 1024
+ * main - Simple UNIX command line interpreter.
+ * Return: 0 on successful execution, -1 on failure.
+ * This program acts as a basic shell,
+ * allowing users to input commands,
+ * displaying a prompt, and executing
+ * the entered command. It supports
+ * basic functionalities such as handling
+ * command lines with arguments,
+ * managing the PATH, implementing built-in
+ * commands like `exit` and `env`,
+ * and more.
+ */
 
 int main(void)
 {
-
-	char *command;
 	char buffer[BUFFER_SIZE];
-	int status;
-	pid_t pid;
 
 	while (1)
 	{
 		printf("#cisfun$ ");
 		if (!fgets(buffer, BUFFER_SIZE, stdin))
-		{
-			printf("\n");
 			break;
-		}
 
-		command = strtok(buffer, "\n");
+		buffer[strcspn(buffer, "\n")] = '\0';
 
-		if (strcmp(command, "exit") == 0)
-		{
-			printf("Exiting shell...\n");
+		if (strcmp(buffer, "exit") == 0)
 			break;
-		}
 
-		pid = fork();
+		pid_t pid = fork();
 
 		if (pid == -1)
 		{
@@ -68,20 +45,14 @@ int main(void)
 		}
 		else if (pid == 0)
 		{
-			char *args[2];
-			args[0] = command;
-			args[1] = NULL;
-			if (execvp(args[0], args) == -1)
-			{
-				perror("execvp");
-				exit(EXIT_FAILURE);
-			}
+			execlp(buffer, buffer, NULL);
+			perror("exec");
+			exit(EXIT_FAILURE);
 		}
 		else
 		{
-			waitpid(pid, &status, 0);
+			waitpid(pid, NULL, 0);
 		}
 	}
-
 	return (0);
 }
